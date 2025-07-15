557f284e71a775aec569680481eaa5ff99867e2a
Deepak <deepakkushwaha5945@gmail.com>
2025-07-15 13:47:09 -0500

    First commit

---
+++ index.js
@@ -0,0 +1,21 @@
+let btn=document.querySelector('button');
+btn.addEventListener('click',function()
+{
+let h=document.querySelector('h2');
+// h.innerText= ;
+
+let body=document.querySelector('body');
+body.style.backgroundColor='black';
+
+let div=document.querySelector('div');
+div.style.backgroundColor='white';
+
+
+});
+
+let btn1=document.querySelector('#button');
+btn1.addEventListener('click',function()
+{
+let body=document.querySelector('body');
+body.style.backgroundColor='white';
+});
